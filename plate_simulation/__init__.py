8fbbfe3e219c32d4fc5180f7e4c32e26ca992ba4
dominiquef <dominiquef@mirageoscience.com>
2024-07-10 08:41:33 -0700

    Lock files

--- plate_simulation/__init__.py
+++ plate_simulation/__init__.py
@@ -15,13 +15,9 @@ from pathlib import Path
 __version__ = "0.1.0-beta.2"
 
 
+from geoapps_utils.importing import assets_path as assets_path_impl
+
+
 def assets_path() -> Path:
     """Return the path to the assets folder."""
-
-    parent = Path(__file__).parent
-    folder_name = f"{parent.name}-assets"
-    assets_folder = parent.parent / folder_name
-    if not assets_folder.is_dir():
-        raise RuntimeError(f"Assets folder not found: {assets_folder}")
-
-    return assets_folder
+    return assets_path_impl(__file__)

ef1135afdbbc10f92dce0df436b46a305290410d
SophieCurinier <sophiec@mirageoscience.com>
2024-07-29 19:54:25 -0400

    [DEVOPS-452] Mypy fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,10 +14,10 @@ release = "2024"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = []  # type: list[str]
 
 templates_path = ["_templates"]
-exclude_patterns = []
+exclude_patterns = []  # type: list[str]
 
 
 # -- Options for HTML output -------------------------------------------------
